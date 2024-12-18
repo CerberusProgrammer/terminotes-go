ca85d0b31c05a404af536393a27057a5c28f4420
Omar Flores <omar.desarrollo@gmtransporterp.com>
2024-12-18 12:52:07 -0800

    Add error constants for command handling in note management

---
+++ src/commands/errors.go
@@ -0,0 +1,9 @@
+package commands
+
+const (
+	ErrExpectedAddCommand  = "expected 'add' command"
+	ErrExpectedListCommand = "expected 'list' command"
+	ErrContentRequired     = "Content is required"
+	ErrEmptyCommands       = "No commands provided, so nothing to do"
+	ErrUnknownCommand      = "Unknown command, I don't know what to do"
+)
