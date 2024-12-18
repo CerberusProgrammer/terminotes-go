cd57d6fd9b586026b165430369be5fd7dd7217c9
Omar Flores <omar.desarrollo@gmtransporterp.com>
2024-12-18 14:29:27 -0800

    Add ViewCommand function to display notes with customizable fields

---
+++ src/commands/view/command_view_controller.go
@@ -0,0 +1,51 @@
+package view
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	sqlitemanager "terminotes/src/data/sqlite-manager"
+	"terminotes/src/utils"
+)
+
+func ViewCommand(args []string) {
+	viewCmd := flag.NewFlagSet("view", flag.ExitOnError)
+	id := viewCmd.Int("id", 0, "ID of the note to view")
+	showID := viewCmd.Bool("show-id", false, "Show ID")
+	showTitle := viewCmd.Bool("show-title", false, "Show Title")
+	showContent := viewCmd.Bool("show-content", false, "Show Content")
+	showCreatedAt := viewCmd.Bool("show-createdAt", false, "Show Created At")
+
+	viewCmd.Parse(args)
+
+	if *id == 0 {
+		fmt.Println("ID is required")
+		viewCmd.PrintDefaults()
+		return
+	}
+
+	columns := []string{}
+	if *showID {
+		columns = append(columns, "id")
+	}
+	if *showTitle {
+		columns = append(columns, "title")
+	}
+	if *showContent {
+		columns = append(columns, "content")
+	}
+	if *showCreatedAt {
+		columns = append(columns, "created_at")
+	}
+
+	if len(columns) == 0 {
+		columns = []string{"id", "title", "content", "created_at"}
+	}
+
+	note, err := sqlitemanager.SelectNoteByID(*id, columns)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	utils.PrintNote(note, columns)
+}
