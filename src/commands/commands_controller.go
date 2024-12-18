6134c968909544a776bf13d4f3a6e38dacc45d6a
Omar Flores <omar.desarrollo@gmtransporterp.com>
2024-12-18 13:02:18 -0800

    Remove ListCommand function to streamline command handling

--- src/commands/commands_controller.go
+++ src/commands/commands_controller.go
@@ -26,42 +26,6 @@ func AddCommand(args []string) {
 	}
 }
 
-func ListCommand(args []string) {
-	listCmd := flag.NewFlagSet("list", flag.ExitOnError)
-	showID := listCmd.Bool("id", false, "Show ID")
-	showTitle := listCmd.Bool("title", false, "Show Title")
-	showContent := listCmd.Bool("content", false, "Show Content")
-	showCreatedAt := listCmd.Bool("createdAt", false, "Show Created At")
-
-	listCmd.Parse(args)
-
-	columns := []string{}
-	if *showID {
-		columns = append(columns, "id")
-	}
-	if *showTitle {
-		columns = append(columns, "title")
-	}
-	if *showContent {
-		columns = append(columns, "content")
-	}
-	if *showCreatedAt {
-		columns = append(columns, "created_at")
-	}
-
-	if len(columns) == 0 {
-		columns = []string{"id", "title", "content", "created_at"}
-	}
-
-	notes, err := sqlitemanager.SelectNotes(columns)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("ID | Title | Content | Created At")
-	fmt.Println(notes)
-}
-
 func HandleCommands() {
 	if len(flag.Args()) < 1 {
 		fmt.Println(ErrEmptyCommands)
