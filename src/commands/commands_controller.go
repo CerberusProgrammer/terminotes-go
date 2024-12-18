58039d8dd712506332034f852f92fcbd3442b523
Omar Flores <omar.desarrollo@gmtransporterp.com>
2024-12-18 14:29:13 -0800

    Remove AddCommand function to streamline command handling

--- src/commands/commands_controller.go
+++ src/commands/commands_controller.go
@@ -3,28 +3,7 @@ package commands
 import (
 	"flag"
 	"fmt"
-	"log"
-	sqlitemanager "terminotes/src/data/sqlite-manager"
 )
-
-func AddCommand(args []string) {
-	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
-	title := addCmd.String("t", "Untitled", "Title of the note (optional)")
-	content := addCmd.String("c", "", "Content of the note (required)")
-
-	addCmd.Parse(args)
-
-	if *content == "" {
-		fmt.Println(ErrContentRequired)
-		addCmd.PrintDefaults()
-		return
-	}
-
-	err := sqlitemanager.CreateNote(*title, *content)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
 
 func HandleCommands() {
 	if len(flag.Args()) < 1 {
