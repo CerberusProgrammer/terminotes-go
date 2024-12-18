bd2e9d3cbdcd9efb106a63cf0fc08582c9396095
Omar Flores <omar.desarrollo@gmtransporterp.com>
2024-12-18 15:00:38 -0800

    Add multiline input support for note content in AddCommand

--- src/commands/add/command_add_controllger.go
+++ src/commands/add/command_add_controllger.go
@@ -1,8 +1,12 @@
 package add
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
 	"log"
+	"os"
+	"strings"
 	sqlitemanager "terminotes/src/data/sqlite-manager"
 )
 
@@ -10,16 +14,43 @@ func AddCommand(args []string) {
 	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
 	title := addCmd.String("t", "Untitled", "Title of the note (optional)")
 	content := addCmd.String("c", "", "Content of the note (required)")
+	multiline := addCmd.Bool("m", false, "Enable multiline input for the content")
 
 	addCmd.Parse(args)
+
+	if *multiline {
+		fmt.Println("Enter your note content. Type ':wq' on a new line to save and exit.")
+		*content = readMultilineInput()
+	}
 
 	if *content == "" {
 		addCmd.PrintDefaults()
 		return
 	}
 
-	err := sqlitemanager.CreateNote(*title, *content)
+	id, err := sqlitemanager.CreateNote(*title, *content)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Printf("Note %d created successfully\n", id)
+}
+
+func readMultilineInput() string {
+	reader := bufio.NewReader(os.Stdin)
+	var lines []string
+
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		line = strings.TrimSpace(line)
+		if line == ":wq" {
+			break
+		}
+		lines = append(lines, line)
+	}
+
+	return strings.Join(lines, "\n")
 }
