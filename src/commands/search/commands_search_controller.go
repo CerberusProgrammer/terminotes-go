f45cebaf74801225cdd414ce36452d56d8484c67
Omar Flores <omar.desarrollo@gmtransporterp.com>
2024-12-19 07:04:24 -0800

    Add search command to retrieve notes based on query parameters

---
+++ src/commands/search/commands_search_controller.go
@@ -0,0 +1,58 @@
+package search
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+	sqlitemanager "terminotes/src/data/sqlite-manager"
+	"terminotes/src/utils"
+)
+
+func SearchCommand(args []string) {
+	searchCmd := flag.NewFlagSet("search", flag.ExitOnError)
+	query := searchCmd.String("query", "", "Text to search for")
+	showID := searchCmd.Bool("show-id", false, "Show ID")
+	showTitle := searchCmd.Bool("show-title", false, "Show Title")
+	showContent := searchCmd.Bool("show-content", false, "Show Content")
+	showCreatedAt := searchCmd.Bool("show-createdAt", false, "Show Created At")
+
+	searchCmd.Parse(args)
+
+	if *query == "" {
+		fmt.Println("Query text is required")
+		searchCmd.PrintDefaults()
+		return
+	}
+
+	columns := []string{}
+	if *showID {
+		columns = append(columns, "id")
+	}
+	if *showTitle {
+		columns = append(columns, "title")
+	}
+	if *showContent {
+		columns = append(columns, "content")
+	}
+	if *showCreatedAt {
+		columns = append(columns, "created_at")
+	}
+
+	if len(columns) == 0 {
+		columns = []string{"id", "title", "content", "created_at"}
+	}
+
+	results, err := sqlitemanager.SearchNotes(*query, columns)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if results == "" {
+		fmt.Println("No results found")
+		return
+	}
+
+	rows := strings.Split(results, "\n")
+	utils.PrintTable(columns, rows)
+}
