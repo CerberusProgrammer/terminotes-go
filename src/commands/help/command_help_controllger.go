8bc9a55f95d94303d802e7f30c65f5d3db67721b
Omar Flores <omar.desarrollo@gmtransporterp.com>
2024-12-19 09:36:28 -0800

    Display available commands with descriptions in HelpCommand

--- src/commands/help/command_help_controllger.go
+++ src/commands/help/command_help_controllger.go
@@ -2,8 +2,13 @@ package help
 
 import (
 	"fmt"
+	"terminotes/src/data"
 )
 
 func HelpCommand(args []string) {
 	fmt.Println("Available commands:")
+
+	for _, command := range data.CommandsInfo {
+		fmt.Printf("%s - %s\n", command.Name, command.Description)
+	}
 }
