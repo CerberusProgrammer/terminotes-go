6b709e2ca37cc23d2f0c3e2818ba43063e11a747
sazardev <sazardev@gmail.com>
2024-12-25 08:43:30 -0800

    Enhance error message for empty commands to suggest using 'help' command

--- src/commands/errors.go
+++ src/commands/errors.go
@@ -4,6 +4,6 @@ const (
 	ErrExpectedAddCommand  = "expected 'add' command"
 	ErrExpectedListCommand = "expected 'list' command"
 	ErrContentRequired     = "Content is required"
-	ErrEmptyCommands       = "No commands provided, so nothing to do"
+	ErrEmptyCommands       = "No commands provided, so nothing to do, but you can run 'help' command to see available commands 😊"
 	ErrUnknownCommand      = "Unknown command, I don't know what to do"
 )
