8980bd17da582062398da62f59027f4cadb65585
Omar Flores <omar.desarrollo@gmtransporterp.com>
2025-01-02 12:56:57 -0800

    Replace SQLite manager with JSON manager for table creation

--- src/main.go
+++ src/main.go
@@ -4,19 +4,15 @@ import (
 	"flag"
 	"log"
 	"terminotes/src/commands"
-	sqlitemanager "terminotes/src/data/sqlite-manager"
+	jsonmanager "terminotes/src/data/json-manager"
 )
 
 func main() {
-	err := sqlitemanager.CreateTable()
+	err := jsonmanager.CreateTable()
 	if err != nil {
 		log.Fatalf("Error creating table: %v", err)
 	}
-	/*
-		- flag.Parse(): Analiza los argumentos de la línea de comandos y los almacena internamente.
-		- flag.Args(): Devuelve los argumentos no procesados como una lista de cadenas.
-		- commands.HandleCommands(): Accede a flag.Args() para determinar qué comando se pasó y maneja la lógica correspondiente.
-	*/
+
 	flag.Parse()
 	commands.HandleCommands()
 }
